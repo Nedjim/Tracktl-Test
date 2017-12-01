278b617fa761c2704c0bb4b62f9a069389cc9e7e
Nedjim <nedjim.danimon@gmail.com>
2017-12-01 23:52:39 +0100

    ADD Order by id and vote functions

--- app/components/Container.js
+++ app/components/Container.js
@@ -1,12 +1,62 @@
 import React from 'react';
 import Playlist from './Playlist';
+import data from '../data/data.json';
 
 export default class Container extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            data : 'vide',
+            list : []
+        }
+    }
+
+    componentWillMount(){
+        if(data.playlist.length != 0)  {
+            this.setState({data: data.playlist});
+        }
+    }
+    
+    orderByPriority(tmpList){
+
+    }
+    orderByVote(tmpList){ 
+        return tmpList.sort((a, b) => b['votes'].count - a['votes'].count);
+    }
+
+    orderById(tmpList){
+        return tmpList.sort((a, b) => a.id - b.id );
+    }
+
+    orderPlaylist(){
+        let tmpList = this.state.data;
+
+        tmpList = this.orderByVote(tmpList);
+
+        console.log(tmpList);
+        //  tmpList.map(e=>{
+        //     console.log(e['votes'].count)
+        // })
+    }
+
     render(){
-        return (
-            <div>
-                <Playlist />
-            </div>
-        )
+
+        this.orderPlaylist();
+
+        if(this.state.data == 'vide'){
+            return (
+                <div>
+                    <h3>La tracklist est vide</h3>
+                    <p>Ajouter des titres depuis la recherche</p>
+                </div>
+                )
+        }
+        else {
+            return (
+                <div>
+                    <Playlist />
+                </div>
+            )
+        }
     }
 }
--- app/components/Playlist.js
+++ app/components/Playlist.js
@@ -7,31 +7,17 @@ export default class Playlist extends React.Component {
     constructor(props){
         super(props);
         this.state = {
-            playlist: 'vide'
+            playlist: data.playlist
         }
     }
 
-    componentWillMount(){
-        if(data.playlist.length != 0)  this.setState({playlist: data.playlist});
-    }
-    
     render() {
-        if(this.state.playlist == 'vide'){
-            return (
-                <div>
-                    <h3>La tracklist est vide</h3>
-                    <p>Ajouter des titres depuis la recherche</p>
-                </div>
-                )
-        }
-        else {
-            return (
-                <div id='playlist'>
-                    {this.state.playlist.map( e => (
-                        <Track key={e.id} details={e}/>
-                    ))}
-                </div>
-            )
-        }
+        return (
+            <div id='playlist'>
+                {this.state.playlist.map( e => (
+                    <Track key={e.id} details={e}/>
+                ))}
+            </div>
+        )
     }
 }
